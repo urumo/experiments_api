3149008b19ebcd1f90f62c4e8b705ecaa59100ce
Aram <aramhrptn@hotmail.com>
2023-12-28 10:47:43 +0400

    added default scope for distributions and optional seeds

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Distribution < ApplicationRecord
+  default_scope { where.not(key: nil).or(where.not(value: nil)) }
   def readonly?
     true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,20 +12,27 @@
 
 User.create!(email: 'admin@email.com', password: 'P@ssw0rd', role: :admin)
 User.create!(email: 'user@email.com', password: 'P@ssw0rd', role: :user)
-#
-# Experiment.create!(key: 'experiment_1', value: 'a', chance: 10)
-# Experiment.create!(key: 'experiment_1', value: 'b', chance: 10)
-# Experiment.create!(key: 'experiment_1', value: 'c', chance: 10)
-# Experiment.create!(key: 'experiment_1', value: 'd', chance: 10)
-# Experiment.create!(key: 'experiment_1', value: 'e', chance: 10)
-# Experiment.create!(key: 'experiment_1', value: 'f', chance: 10)
-# Experiment.create!(key: 'experiment_1', value: 'g', chance: 10)
-# Experiment.create!(key: 'experiment_1', value: 'h', chance: 10)
-# Experiment.create!(key: 'experiment_1', value: 'i', chance: 10)
-# Experiment.create!(key: 'experiment_1', value: 'j', chance: 10)
-#
-# Experiment.create!(key: 'experiment_2', value: 'a', chance: 33.3)
-# Experiment.create!(key: 'experiment_2', value: 'b', chance: 33.3)
-# Experiment.create!(key: 'experiment_2', value: 'c', chance: 33.3)
-#
-# 60_000.times { Device.create! }
+
+ci_env = ENV.fetch('CI_ENV', 'development')
+return if ci_env == 'github-actions'
+
+500.times { Device.create! }
+
+Experiment.create!(key: 'experiment_1', value: 'a', chance: 10)
+Experiment.create!(key: 'experiment_1', value: 'b', chance: 10)
+Experiment.create!(key: 'experiment_1', value: 'c', chance: 10)
+Experiment.create!(key: 'experiment_1', value: 'd', chance: 10)
+Experiment.create!(key: 'experiment_1', value: 'e', chance: 10)
+Experiment.create!(key: 'experiment_1', value: 'f', chance: 10)
+Experiment.create!(key: 'experiment_1', value: 'g', chance: 10)
+Experiment.create!(key: 'experiment_1', value: 'h', chance: 10)
+Experiment.create!(key: 'experiment_1', value: 'i', chance: 10)
+Experiment.create!(key: 'experiment_1', value: 'j', chance: 10)
+
+Experiment.create!(key: 'experiment_2', value: 'a', chance: 33.3)
+Experiment.create!(key: 'experiment_2', value: 'b', chance: 33.3)
+Experiment.create!(key: 'experiment_2', value: 'c', chance: 33.3)
+
+GC.disable
+60_000.times { Device.create! }
+GC.enable
